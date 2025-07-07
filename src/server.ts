70046526284e4ddb79da6585faf988bdde60cee4
IgorSasaki <igor-sasaki@hotmail.com>
2025-07-07 20:31:47 -0300

    feat (server): Implement configuration of server

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import { fastifyCors } from "@fastify/cors";
+import { fastify } from "fastify";
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: 'http://localhost:5173'
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.listen({ port: process.env.PORT ? Number(process.env.PORT) : 3333 })
+  .then(() => console.log('HTTP Server running! 🚀🚀'))
+  .catch((err) => console.error(err))
